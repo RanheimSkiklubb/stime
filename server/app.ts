cfe39986ecad5cb8cc6a4753004ae3e1c9cf5546
oyron <oyron@statoil.com>
2020-01-02 21:54:14 +0100

    Cors on server

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import API from './api/api';
 import * as http from 'http'
 import { Db } from 'mongodb';
@@ -9,7 +10,8 @@ export class App {
     constructor(db: Db) {
         const app = express();
         app.use(bodyParser.json());
-        app.use(express.static('../client/build'))
+        app.use(express.static('../client/build'));
+        app.use(cors());
         app.use('/api', new API(db).router);
         this.expressServer = http.createServer(app);
     }
