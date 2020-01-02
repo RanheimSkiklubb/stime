58c420290e41ed58aa6eea8408c97342d21398f8
oyron <oyron@statoil.com>
2020-01-02 21:54:33 +0100

    Load events from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,30 +4,64 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
+import moment from 'moment';
 
-const Content: React.FC = () => {
+interface Event {
+    id: string;
+    name: string;
+    startTime: Date;
+    participants: string[];
+}
+
+interface Props {
+    events: Event[]
+}
+
+const Content: React.FC<Props> = ({events}: Props) => {
+    
+    const createList = () => events.map(event => 
+    <ListGroup.Item action href="#link1">{moment(event.startTime).format("DD. MMM YYYY")}: {event.name}</ListGroup.Item>
+        );
+
     return (
         <div>
             <h1>Arrangement</h1><br/>
             <ListGroup>
-                <ListGroup.Item action href="#link1">Telenorkarusellen 2020: Renn 1</ListGroup.Item>
-                <ListGroup.Item action href="#link2">Telenorkarusellen 2020: Renn 2</ListGroup.Item>
-                <ListGroup.Item action href="#link3">Telenorkarusellen 2020: Renn 3</ListGroup.Item>
+                {events ? createList() : ""}
             </ListGroup>
         </div>
     );
 }
 
-const App: React.FC = () => {
-    return (
-        <Container fluid={true}>
-            <Row>
-                <Col></Col>
-                <Col className="text-center" style={{"border": "solid 1px"}} xs={9}><Content/></Col>
-                <Col></Col>
-            </Row>
-        </Container>
-    );
+
+
+class App extends React.Component<{}, Props> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {events: props.events};
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/event')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ events: data })
+        })
+        .catch(console.log)
+    }
+
+    render() {
+        return (
+            <Container fluid={true}>
+                <Row>
+                    <Col></Col>
+                    <Col className="text-center" xs={9}><Content events={this.state.events}/></Col>
+                    <Col></Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default App;
