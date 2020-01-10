778446e55ec58ac3fb5694597bf46f671c7eb73c
oyron <oyron@statoil.com>
2020-01-10 11:44:41 +0100

    EventPage hook refactoring

--- client/src/event-page/EventPage.tsx
+++ client/src/event-page/EventPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './EventPage.css';
 import Event from '../model/event';
 import Participant from '../model/participant';
@@ -22,109 +22,114 @@ interface MatchParams {
 }
 
 interface Props {
-    match?: match<MatchParams>
+    match: match<MatchParams>
 }
 
+const EventPage: React.FC<Props> = (props: Props) => {
 
-export default class EventPage extends React.Component<Props, State> {
-    
+    const [event, setEvents] = useState<Event>({id: "", name: "", description: "", startTime: new Date(), registrationStart: new Date(), registrationEnd: new Date(), eventClasses:[], participants: []});
+    const [clubs, setClubs] = useState<Club[]>([]);
 
-    async componentDidMount() {
-        if (!this.props.match) {
-            console.error("Props.match not set!");
-            return;
+    useEffect(() => {
+        const fecthEvents = async () => {
+            const eventId = props.match.params.eventId;
+            const eventBody = await fetch(`http://localhost:3000/api/event/${eventId}`)
+            const eventData = await eventBody.json();
+            const loadedEvent = {
+                id: eventData.id, 
+                name: eventData.name,
+                description: eventData.description,
+                startTime: eventData.startTime, 
+                registrationStart: eventData.registrationStart, 
+                registrationEnd: eventData.registrationEnd, 
+                eventClasses: eventData.eventClasses, 
+                participants: eventData.participants
+            };
+            setEvents(loadedEvent);
         }
-        const eventId = this.props.match.params.eventId;
-        const eventBody = await fetch(`http://localhost:3000/api/event/${eventId}`)
-        const eventData = await eventBody.json();
-        const clubsBody = await fetch('http://localhost:3000/api/club')
-        const clubsData = await clubsBody.json();
-        
-        const event = {
-            id: eventData.id, 
-            name: eventData.name,
-            description: eventData.description,
-            startTime: eventData.startTime, 
-            registrationStart: eventData.registrationStart, 
-            registrationEnd: eventData.registrationEnd, 
-            eventClasses: eventData.eventClasses, 
-            participants: eventData.participants
-        };
-        this.setState({ event, clubs: clubsData });
-        
-    }
+        fecthEvents();
+    }, [props.match]);
 
-    render() {
-        if (!this.state || !this.state.event) return null;
+    useEffect(() => {
+        const fecthEvents = async () => {
+            const clubsBody = await fetch('http://localhost:3000/api/club')
+            const clubsData = await clubsBody.json();
+            setClubs(clubsData)
+        }
+        fecthEvents();
+    }, []);
 
-        const event = this.state.event;
-        const infoTab = (
-            <div>
-                <table className="eventInfo">
-                    <tbody>
-                        <tr><td>Dato:</td><td>{moment(event.startTime).format("DD. MMM YYYY")}</td></tr>
-                        <tr><td>Første start:</td><td>{moment(event.startTime).format("HH:mm")}</td></tr>
-                        <tr><td>Påmeldingsfrist:</td><td>{moment(event.registrationEnd).format("DD. MMM YYYY HH:mm")}</td></tr>
-                        <tr><td>Beskrivelse:</td><td>{event.description}</td></tr>
-                        <tr><td colSpan={2}>
-                            <Table striped bordered size="sm">
-                                <thead>
-                                    <th>Klasse</th>
-                                    <th>Løype</th>
-                                </thead>
-                                <tbody>
-                                    {event.eventClasses.map((ec:EventClass, idx:number) => 
-                                    <tr key={idx}>
-                                        <td>{ec.name}</td>
-                                        <td>{ec.course}</td>
-                                    </tr>)}
-                                </tbody>
-                            </Table></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <hr/>
+    //if (!this.state || !this.state.event) return null;
 
-                <RegistrationModal event={event} clubs={this.state.clubs}/>
-                
-            </div>
-        );
-
-        const participantList = (
-            <Table striped bordered size="sm">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Navn</th>
-                        <th>Klubb</th>
-                        <th>Klasse</th>
-                    </tr>
-                </thead>
+    const infoTab = (
+        <div>
+            <table className="eventInfo">
                 <tbody>
-                    {
-                    event.participants.map((p:Participant, idx:number) => 
-                    <tr key={idx}><td>{idx + 1}</td>
-                        <td>{p.firstName + " " + p.lastName}</td>
-                        <td>{p.club}</td>
-                        <td>{p.eventClass}</td>
-                    </tr>)
-                    }
+                    <tr><td>Dato:</td><td>{moment(event.startTime).format("DD. MMM YYYY")}</td></tr>
+                    <tr><td>Første start:</td><td>{moment(event.startTime).format("HH:mm")}</td></tr>
+                    <tr><td>Påmeldingsfrist:</td><td>{moment(event.registrationEnd).format("DD. MMM YYYY HH:mm")}</td></tr>
+                    <tr><td>Beskrivelse:</td><td>{event.description}</td></tr>
+                    <tr><td colSpan={2}>
+                        <Table striped bordered size="sm">
+                            <thead>
+                                <th>Klasse</th>
+                                <th>Løype</th>
+                            </thead>
+                            <tbody>
+                                {event.eventClasses.map((ec:EventClass, idx:number) => 
+                                <tr key={idx}>
+                                    <td>{ec.name}</td>
+                                    <td>{ec.course}</td>
+                                </tr>)}
+                            </tbody>
+                        </Table></td>
+                    </tr>
                 </tbody>
-            </Table>
-        );
+            </table>
+            <hr/>
 
-        return (
-            <div>
-                <h2>{event.name}</h2>
-                    <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-                    <Tab eventKey="home" title="Informasjon">
-                        {infoTab}
-                    </Tab>
-                    <Tab eventKey="profile" title="Deltakerliste">
-                        {participantList}
-                    </Tab>
-                </Tabs>
-            </div>
-        );
-    }
+            <RegistrationModal event={event} clubs={clubs}/>
+            
+        </div>
+    );
+
+    const participantList = (
+        <Table striped bordered size="sm">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Navn</th>
+                    <th>Klubb</th>
+                    <th>Klasse</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                event.participants.map((p:Participant, idx:number) => 
+                <tr key={idx}><td>{idx + 1}</td>
+                    <td>{p.firstName + " " + p.lastName}</td>
+                    <td>{p.club}</td>
+                    <td>{p.eventClass}</td>
+                </tr>)
+                }
+            </tbody>
+        </Table>
+    );
+
+    return (
+        <div>
+            <h2>{event.name}</h2>
+                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+                <Tab eventKey="home" title="Informasjon">
+                    {infoTab}
+                </Tab>
+                <Tab eventKey="profile" title="Deltakerliste">
+                    {participantList}
+                </Tab>
+            </Tabs>
+        </div>
+    );
+    
 }
+
+export default EventPage;
