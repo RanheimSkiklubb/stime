939c6cb675d92f271758d730ad87fd1743bbf9ee
oyron <oyron@statoil.com>
2020-01-15 22:43:41 +0100

    material ui on event list page

--- client/src/event-list-page/EventListPage.tsx
+++ client/src/event-list-page/EventListPage.tsx
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
-import '../App.css';
-import Table from 'react-bootstrap/Table';
 import moment from 'moment';
 import Event from '../model/event';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 interface State {
     events: Event[]
 }
 
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+});
+
 const EventListPage: React.FC = () => {
     
     const history = useHistory();
     const [events, setEvents] = useState<Event[]>([]);
+    const classes = useStyles();
 
     useEffect(() => {
         const fecthEvents = () => {
@@ -22,12 +36,35 @@ const EventListPage: React.FC = () => {
             .then((data) => setEvents(data))
         }
         fecthEvents();
-      }, []);
+    }, []);
+
  
     return (
         <div>
             <h1>Arrangement</h1><br/>
-            <Table hover>
+
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Dato</TableCell>
+                            <TableCell>Arrangment</TableCell>
+                            <TableCell>Øvelse</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {events.map((event:Event) => (
+                        <TableRow hover key={event.name} onClick={() => history.push(`/event/${event.id}`)}>
+                            <TableCell>{moment(event.startTime).format("DD. MMM YYYY")}</TableCell>
+                            <TableCell>{event.name}</TableCell>
+                            <TableCell>{event.eventType}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
+
+            {/* <BSTable hover>
                 <thead>
                     <tr>
                         <th scope="row">Dato</th>
@@ -42,7 +79,7 @@ const EventListPage: React.FC = () => {
                         </tr>
                     )}
                 </tbody>
-            </Table>
+            </BSTable> */}
         </div>
     );
 
