a617ff0034dfd5e88922c4b7047da9b0065210ad
oyron <oyron@statoil.com>
2020-01-15 23:10:59 +0100

    container component in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import EventListPage from './event-list-page/EventListPage'
 import EventPage from './event-page/EventPage'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App: React.FC = () => {
 
-        return (
-            <Container fluid={true}>
-                <Row>
-                    <Col></Col>
-                    <Col className="text-center" xs={9}>
-                        <Router>
-                            <Switch>
-                                <Route path="/event/:eventId" component={(props:any) => <EventPage {...props} />} />
-                                <Route path="/">
-                                    <EventListPage/>
-                                </Route>
-                            </Switch>
-                        </Router>
-                    </Col>
-                    <Col></Col>
-                </Row>
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <Router>
+                    <Switch>
+                        <Route path="/event/:eventId" component={(props:any) => <EventPage {...props} />} />
+                        <Route path="/">
+                            <EventListPage/>
+                        </Route>
+                    </Switch>
+                </Router>
             </Container>
-        );
-    }
+        </React.Fragment>
+    );
+}
 
 export default App;
