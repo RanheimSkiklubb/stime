b6aa337fb76fbf4ff46d2990b6936dcde2861626
oyron <oyron@statoil.com>
2020-01-17 15:18:20 +0100

    validation in registration page

--- client/src/event-page/RegistrationModal.tsx
+++ client/src/event-page/RegistrationModal.tsx
@@ -1,4 +1,4 @@
-import React, {useState, ChangeEvent, FormEvent} from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -16,6 +16,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import Paper from '@material-ui/core/Paper';
 import Event from '../model/event';
 import Club from '../model/club';
+import * as _ from "lodash";
 
 interface Props {
     event: Event,
@@ -25,29 +26,51 @@ interface Props {
 
 const RegistrationForm: React.FC<Props> = (props: Props) => {
 
-    const [validated, setValidated] = useState(false);
     const [progress, setProgress] = useState(0);
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [club, setClub] = useState("");
     const [eventClass, setEventClass] = useState("Mini");
+    const [firstNameValid, setFirstNameValid] = useState(true);
+    const [lastNameValid, setLastNameValid] = useState(true);
+    const [clubValid, setClubValid] = useState(true);
+    const [formValid, setFormValid] = useState(false);
 
-    const firstNameChange = (event:ChangeEvent<HTMLInputElement>) => setFirstName(event.currentTarget.value);
-    const lastNameChange = (event:ChangeEvent<HTMLInputElement>) => setLastName(event.currentTarget.value);
-    const eventClassChange = (event:ChangeEvent<{ value: unknown }>) => setEventClass(event.target.value as string);
-
-    const handleEdit = () => {
-        setProgress(0);
+    const firstNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.currentTarget.value;
+        setFirstName(value);
+        const newFirstNameValid = value.length > 0;
+        setFirstNameValid(newFirstNameValid);
+        validate(value);
     }
+    const lastNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.currentTarget.value;
+        setLastName(value);
+        const newLastNameValid = value.length > 0;
+        setLastNameValid(newLastNameValid);
+        validate(undefined, value);
+    }
+    const eventClassChange = (event: ChangeEvent<{ value: unknown }>) => {
+        setEventClass(event.target.value as string);
+    }
+    const clubChange = (newClub:string) => {
+        setClub(newClub);
+        const newClubValid = !_.isNil(newClub) && newClub.length > 0;
+        setClubValid(newClubValid);
+        validate(undefined, undefined, newClub);
+    }
+
+    const validate = (newFirstName = firstName, newLastName = lastName, newClub = club) => 
+        setFormValid(!_.isNil(newFirstName) && newFirstName.length > 0 && !_.isNil(newLastName) && newLastName.length > 0 && !_.isNil(newClub) && newClub.length > 0);
 
     const handleRegister = async () => {
         try {
-            await fetch(`http://localhost:3001/api/event/1/participant`, 
-            {
-                method: 'POST', 
-                body: JSON.stringify({firstName, lastName, club, eventClass}),
-                headers: {'Content-Type': 'application/json'}
-            });
+            await fetch(`http://localhost:3001/api/event/1/participant`,
+                {
+                    method: 'POST',
+                    body: JSON.stringify({ firstName, lastName, club, eventClass }),
+                    headers: { 'Content-Type': 'application/json' }
+                });
             props.loadEventCallback();
             setProgress(2);
         }
@@ -55,23 +78,12 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
             console.error(error);
         }
     }
-
     const handleRegisterMore = () => {
         setFirstName("");
         setProgress(0);
     }
-
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        const form = event.currentTarget;
-        event.preventDefault();
-        event.stopPropagation();
-        if (form.checkValidity()) {
-            setProgress(1);
-        }
-        else {
-            setValidated(true); //Trigger display of validation responses in form
-        }
-    };
+    const handleEdit = () => setProgress(0);
+    const handleNext = () => setProgress(1);
 
     const useStyles = makeStyles((theme: Theme) =>
         createStyles({
@@ -84,30 +96,30 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
         }),
     );
     const classes = useStyles();
-    let tempValue:string;
+    let tempValue: string;
     const form = (
-        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
-            <TextField id="firstName" label="Fornavn" value={firstName} onChange={firstNameChange}/>
-            <TextField id="lastName" label="Etternavn" value={lastName} onChange={lastNameChange}/>
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="firstName" label="Fornavn" value={firstName} onChange={firstNameChange} error={!firstNameValid} />
+            <TextField id="lastName" label="Etternavn" value={lastName} onChange={lastNameChange} error={!lastNameValid} />
             <Autocomplete
                 id="club"
                 freeSolo
-                disableClearable
                 value={club}
                 options={props.clubs.map(club => club.name)}
                 onChange={(event: any, newValue: any | null) => {
-                    setClub(newValue);
+                    clubChange(newValue);
                 }}
-                onInputChange={(event: any, newValue:any) => {
+                onInputChange={(event: any, newValue: any) => {
                     tempValue = newValue; //neccessary due to bug in <Autocomplete>. Should be able to set state directlu
+                    validate(undefined, undefined, newValue);
                 }}
                 onClose={(event: any) => {
-                    tempValue && setClub(tempValue);
-                }
-
-                }
+                    if (tempValue) {
+                        clubChange(tempValue);
+                    }
+                }}
                 renderInput={params => (
-                    <TextField {...params} label="Klubb" margin="normal" fullWidth InputProps={{ ...params.InputProps, type: 'search' }}/>
+                    <TextField {...params} label="Klubb" margin="normal" fullWidth InputProps={{ ...params.InputProps, type: 'search' }} error={!clubValid} />
                 )}
             />
             <FormControl>
@@ -116,14 +128,14 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
                     {props.event.eventClasses.map(eventClass => (<MenuItem value={eventClass.name} key={eventClass.name}>{`${eventClass.name} (${eventClass.course})`}</MenuItem>))}
                 </Select>
             </FormControl>
-            <Button className="float-right" variant="contained" color="primary" type="submit">Neste</Button>
+            <Button className="float-right" variant="contained" color="primary" onClick={handleNext} disabled={!formValid}>Neste</Button>
         </form>
     );
 
     const step1 = (
         <div>
             <p>Du har registert følgende</p>
-            
+
             <TableContainer component={Paper}>
                 <Table>
                     <TableBody>
@@ -146,7 +158,7 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
                     </TableBody>
                 </Table>
             </TableContainer>
-            
+
             <Button variant="contained" color="primary" onClick={handleEdit}>Endre</Button>
             <Button variant="contained" color="primary" className="float-right" onClick={handleRegister}>Meld på</Button>
         </div>
@@ -166,35 +178,35 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
         return step1;
     }
     return step2;
-    
+
 }
 
 function rand() {
     return Math.round(Math.random() * 20) - 10;
-  }
-  
-  function getModalStyle() {
+}
+
+function getModalStyle() {
     const top = 50 + rand();
     const left = 50 + rand();
-  
-    return {
-      top: `${top}%`,
-      left: `${left}%`,
-      transform: `translate(-${top}%, -${left}%)`,
-    };
-  }
 
-  const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    paper: {
-      position: 'absolute',
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-  }),
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+    }),
 );
 
 const RegistrationModal: React.FC<Props> = (props: Props) => {
@@ -204,19 +216,14 @@ const RegistrationModal: React.FC<Props> = (props: Props) => {
     const [modalStyle] = React.useState(getModalStyle);
     const classes = useStyles();
 
-    return (  
+    return (
         <>
             <Button variant="contained" color="primary" className="marginTop20" onClick={handleShow}>Påmelding</Button>
-
             <Modal open={show} onClose={handleClose}>
                 <div style={modalStyle} className={classes.paper}>
                     <h3>Påmelding</h3>
-                    
-                    <RegistrationForm event={props.event} clubs={props.clubs} loadEventCallback={props.loadEventCallback}/>
-                
-                
+                    <RegistrationForm event={props.event} clubs={props.clubs} loadEventCallback={props.loadEventCallback} />
                     <Button variant="contained" color="default" onClick={handleClose}>Lukk</Button>
-                    
                 </div>
             </Modal>
         </>
