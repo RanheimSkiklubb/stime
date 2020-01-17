e1df322680ddf41a9ab60732bbd708cb72cc329f
oyron <oyron@statoil.com>
2020-01-17 12:30:09 +0100

    convert registration to material ui

--- client/src/event-page/RegistrationModal.tsx
+++ client/src/event-page/RegistrationModal.tsx
@@ -1,12 +1,21 @@
 import React, {useState, ChangeEvent, FormEvent} from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Table from 'react-bootstrap/Table';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import TableContainer from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
 import Event from '../model/event';
 import Club from '../model/club';
-import './EventPage.css';
-
 
 interface Props {
     event: Event,
@@ -20,13 +29,12 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
     const [progress, setProgress] = useState(0);
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [club, setClub] = useState(props.clubs[0].name);
+    const [club, setClub] = useState("");
     const [eventClass, setEventClass] = useState("Mini");
 
     const firstNameChange = (event:ChangeEvent<HTMLInputElement>) => setFirstName(event.currentTarget.value);
     const lastNameChange = (event:ChangeEvent<HTMLInputElement>) => setLastName(event.currentTarget.value);
-    const clubChange = (event:ChangeEvent<HTMLInputElement>) => setClub(event.currentTarget.value);
-    const eventClassChange = (event:ChangeEvent<HTMLInputElement>) => setEventClass(event.currentTarget.value);
+    const eventClassChange = (event:ChangeEvent<{ value: unknown }>) => setEventClass(event.target.value as string);
 
     const handleEdit = () => {
         setProgress(0);
@@ -65,66 +73,89 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
         }
     };
 
+    const useStyles = makeStyles((theme: Theme) =>
+        createStyles({
+            root: {
+                '& > *': {
+                    margin: theme.spacing(1),
+                    width: 200,
+                },
+            },
+        }),
+    );
+    const classes = useStyles();
+    let tempValue:string;
     const form = (
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicFirstName">
-                <Form.Label>Fornavn</Form.Label>
-                <Form.Control type="text" required placeholder="Skriv inn for- og mellomnavn" onChange={firstNameChange} value={firstName}/>
-            </Form.Group>
-            <Form.Group controlId="formBasicLastName">
-                <Form.Label>Etternavn</Form.Label>
-                <Form.Control type="text" required placeholder="Skriv inn etternavn" onChange={lastNameChange} value={lastName}/>
-            </Form.Group>
-            <Form.Group controlId="formBasicClub">
-                <Form.Label>Klubb</Form.Label>
-                <Form.Control as="select" onChange={clubChange} value={club}>
-                    {props.clubs.map(c => (<option key={c.name}>{c.name}</option>))}
-                </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="formBasicEventClass">
-                <Form.Label>Velg klasse</Form.Label>
-                <Form.Control as="select" onChange={eventClassChange} value={eventClass}>
-                    {props.event.eventClasses.map(eventClass => (<option value={eventClass.name} key={eventClass.name}>{`${eventClass.name} (${eventClass.course})`}</option>))}
-                </Form.Control>
-            </Form.Group>
-            <Button className="float-right" variant="primary" type="submit">Neste</Button>
-        </Form>
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField id="firstName" label="Fornavn" value={firstName} onChange={firstNameChange}/>
+            <TextField id="lastName" label="Etternavn" value={lastName} onChange={lastNameChange}/>
+            <Autocomplete
+                id="club"
+                freeSolo
+                disableClearable
+                value={club}
+                options={props.clubs.map(club => club.name)}
+                onChange={(event: any, newValue: any | null) => {
+                    setClub(newValue);
+                }}
+                onInputChange={(event: any, newValue:any) => {
+                    tempValue = newValue; //neccessary due to bug in <Autocomplete>. Should be able to set state directlu
+                }}
+                onClose={(event: any) => {
+                    tempValue && setClub(tempValue);
+                }
+
+                }
+                renderInput={params => (
+                    <TextField {...params} label="Klubb" margin="normal" fullWidth InputProps={{ ...params.InputProps, type: 'search' }}/>
+                )}
+            />
+            <FormControl>
+                <InputLabel id="event-class-label">Klasse</InputLabel>
+                <Select labelId="event-class-label" id="eventClass" value={eventClass} onChange={eventClassChange}>
+                    {props.event.eventClasses.map(eventClass => (<MenuItem value={eventClass.name} key={eventClass.name}>{`${eventClass.name} (${eventClass.course})`}</MenuItem>))}
+                </Select>
+            </FormControl>
+            <Button className="float-right" variant="contained" color="primary" type="submit">Neste</Button>
+        </form>
     );
 
     const step1 = (
         <div>
             <p>Du har registert følgende</p>
-            <div>
-            <Table bordered size="sm">
-                <tbody>
-                    <tr>
-                        <td>Fornavn</td>
-                        <td>{firstName}</td>
-                    </tr>
-                    <tr>
-                        <td>Etternavn</td>
-                        <td>{lastName}</td>
-                    </tr>
-                    <tr>
-                        <td>Klubb</td>
-                        <td>{club}</td>
-                    </tr>
-                    <tr>
-                        <td>Klasse</td>
-                        <td>{eventClass}</td>
-                    </tr>
-                </tbody>
-            </Table>
-            </div>
-            <Button variant="primary" onClick={handleEdit}>Endre</Button>
-            <Button variant="primary" className="float-right" onClick={handleRegister}>Meld på</Button>
+            
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Fornavn</TableCell>
+                            <TableCell>{firstName}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Etternavn</TableCell>
+                            <TableCell>{lastName}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Klubb</TableCell>
+                            <TableCell>{club}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Klasse</TableCell>
+                            <TableCell>{eventClass}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            
+            <Button variant="contained" color="primary" onClick={handleEdit}>Endre</Button>
+            <Button variant="contained" color="primary" className="float-right" onClick={handleRegister}>Meld på</Button>
         </div>
     );
 
     const step2 = (
         <div>
             <p>Din påmelding er registrert!</p>
-            <Button variant="primary" className="float-right" onClick={handleRegisterMore}>Meld på flere</Button>
+            <Button variant="contained" color="primary" className="float-right" onClick={handleRegisterMore}>Meld på flere</Button>
         </div>
     );
 
@@ -138,26 +169,55 @@ const RegistrationForm: React.FC<Props> = (props: Props) => {
     
 }
 
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+  }
+  
+  function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
+  const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }),
+);
 
 const RegistrationModal: React.FC<Props> = (props: Props) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);    
+    const handleShow = () => setShow(true);
+    const [modalStyle] = React.useState(getModalStyle);
+    const classes = useStyles();
 
     return (  
         <>
-            <Button variant="primary" className="marginTop20" onClick={handleShow}>Påmelding</Button>
+            <Button variant="contained" color="primary" className="marginTop20" onClick={handleShow}>Påmelding</Button>
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Påmelding</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
+            <Modal open={show} onClose={handleClose}>
+                <div style={modalStyle} className={classes.paper}>
+                    <h3>Påmelding</h3>
+                    
                     <RegistrationForm event={props.event} clubs={props.clubs} loadEventCallback={props.loadEventCallback}/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>Lukk</Button>
-                </Modal.Footer>
+                
+                
+                    <Button variant="contained" color="default" onClick={handleClose}>Lukk</Button>
+                    
+                </div>
             </Modal>
         </>
     );
