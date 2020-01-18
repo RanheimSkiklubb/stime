1ecafff0b00b29ab3a9c42ff04280cf1c945fbb5
oyron <oyron@statoil.com>
2020-01-18 22:01:54 +0100

    Change to system font

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
+import { createMuiTheme, MuiThemeProvider} from "@material-ui/core/styles";
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
+  });
+
+ReactDOM.render(
+    <MuiThemeProvider theme = { theme }>
+        <App />
+    </MuiThemeProvider>, 
+ document.getElementById('root'));
