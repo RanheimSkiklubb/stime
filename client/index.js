09a91f37b672b25cbe3e8e341daae94b8c18da9e
oyron <oyron@statoil.com>
2020-01-21 16:01:09 +0100

    remove obsolete files

--- client/index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-
-const port = process.env.PORT|3001;
-const app = express();
-app.use(express.static('build'));
-app.listen(port, () => console.log(`Library server is running on ${port}`));
