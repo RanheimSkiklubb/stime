31efb60dc14e048e8d093ed66125c10ff53a8f9c
Tor Erik Rabben <toera@equinor.com>
2026-04-21 20:56:48 +0200

    Migrate from CRA to Vite

--- client/src/components/Firebase/index.ts
+++ client/src/components/Firebase/index.ts
@@ -34,12 +34,12 @@ interface FirebaseConfig {
 }
 
 export const config: FirebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    apiKey: import.meta.env.REACT_APP_API_KEY,
+    authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: import.meta.env.REACT_APP_DATABASE_URL,
+    projectId: import.meta.env.REACT_APP_PROJECT_ID,
+    storageBucket: import.meta.env.REACT_APP_STOREAGE_BUCKET,
+    messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 const firebaseApp = initializeApp(config);
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="react-scripts" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly REACT_APP_API_KEY?: string;
+    readonly REACT_APP_AUTH_DOMAIN?: string;
+    readonly REACT_APP_DATABASE_URL?: string;
+    readonly REACT_APP_PROJECT_ID?: string;
+    readonly REACT_APP_STOREAGE_BUCKET?: string;
+    readonly REACT_APP_MESSAGING_SENDER_ID?: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
